00133ae360f27fc41fcd6b28ab4d34f4052f6a26
huhuanming <workboring@gmail.com>
2025-02-11 12:22:49 +0800

    Update App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,82 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { PrivyProvider } from '@privy-io/react-auth';
+import {
+  PrivyProvider,
+  useLoginWithEmail,
+  usePrivy,
+} from '@privy-io/react-auth';
 import icon from '../../assets/icon.svg';
+
+export type IPrivyState =
+  | { status: 'initial' }
+  | {
+      status: 'error';
+      error: Error | null;
+    }
+  | { status: 'sending-code' }
+  | { status: 'awaiting-code-input' }
+  | { status: 'submitting-code' }
+  | { status: 'done' };
+
+export interface IUsePrivyUniversalV2 {
+  logout: () => Promise<void>;
+  isReady: boolean;
+  getAccessToken: () => Promise<string | null>;
+  useLoginWithEmail: (props?: {
+    onComplete?: () => void;
+    onError?: (error: any) => void;
+  }) => {
+    state: IPrivyState;
+    sendCode: (args: { email: string }) => Promise<void>;
+    loginWithCode: (args: { code: string; email?: string }) => Promise<void>;
+  };
+  authenticated: boolean;
+  user?: {
+    id: string;
+    email: string;
+  };
+}
+
+export function usePrivyUniversalV2(): IUsePrivyUniversalV2 {
+  const { logout, ready, getAccessToken, authenticated, user } = usePrivy();
+
+  return {
+    useLoginWithEmail: (args) => {
+      const { onComplete, onError } = args || {};
+      const { sendCode, loginWithCode, state } = useLoginWithEmail({
+        onComplete,
+        onError: (error) => {
+          onError?.(error);
+        },
+      });
+
+      return {
+        state,
+        sendCode: async (...sendCodeArgs) => {
+          await sendCode(...sendCodeArgs);
+        },
+        loginWithCode: async (...loginWithCodeArgs) => {
+          await loginWithCode(...loginWithCodeArgs);
+        },
+      };
+    },
+    logout,
+    isReady: ready,
+    getAccessToken,
+    authenticated,
+    user: authenticated
+      ? {
+          id: user?.id || '',
+          email: user?.email?.address || '',
+        }
+      : undefined,
+  };
+}
+
+function Login() {
+  const { user, isReady } = usePrivyUniversalV2();
+  console.log('result---', user, isReady);
+  return null;
+}
 
 function Hello() {
   const appId = 'cm5599w1609v7kfpecfmser60';
@@ -49,6 +125,7 @@ function Hello() {
           </a>
         </div>
       </div>
+      <Login />
     </PrivyProvider>
   );
 }
